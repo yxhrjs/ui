1111ec34a609b8c5a7da7b4d92b3bcceca71f8bd
Macarubbo <c.f.macarubbo@accenture.com>
2024-05-27 01:31:44 +0800

    initial commit

---
+++ components/empty-screen.tsx
@@ -0,0 +1,23 @@
+export function EmptyScreen() {
+  return (
+    <div className="mx-auto max-w-2xl px-4">
+      <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
+        <h1 className="text-lg font-semibold">
+          Welcome to Mandy AI!
+        </h1>
+        <p className="leading-normal text-muted-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        { /*<p className="leading-normal text-muted-foreground">
+          It uses{' '}
+          <ExternalLink href="https://vercel.com/blog/ai-sdk-3-generative-ui">
+            React Server Components
+          </ExternalLink>{' '}
+          to combine text with generative UI as output of the LLM. The UI state
+          is synced through the SDK so the model is aware of your interactions
+          as they happen.
+        </p>*/}
+      </div>
+    </div>
+  )
+}
