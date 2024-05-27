7d0359ac732b66491237aaec960942c32c51d7c6
Macarubbo <c.f.macarubbo@accenture.com>
2024-05-27 09:54:04 +0800

    design updated

--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -19,11 +19,11 @@ export function ChatList({ messages, session, isShared }: ChatList) {
     <div className="relative mx-auto max-w-2xl ">
       {!isShared && !session ? (
         <>
-          <div className="group relative mb-4 flex items-start md:-ml-12">
+          { /*<div className="group relative mb-4 flex items-start md:-ml-12">
             <div className="bg-background flex size-[25px] shrink-0 select-none items-center justify-center rounded-md border shadow-sm">
               <ExclamationTriangleIcon />
             </div>
-            { /*<div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
+            <div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
               <p className="text-muted-foreground leading-normal">
                 Please{' '}
                 <Link href="/login" className="underline">
@@ -35,8 +35,8 @@ export function ChatList({ messages, session, isShared }: ChatList) {
                 </Link>{' '}
                 to save and revisit your chat history!
               </p>
-            </div> */ }
-          </div>
+            </div>
+          </div> */ }
           <Separator className="my-4" />
         </>
       ) : null}
