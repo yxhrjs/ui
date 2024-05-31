97117b8848e8dc8d7cac5444e36a695a360abe2f
Macarubbo <c.f.macarubbo@accenture.com>
2024-05-31 14:23:54 +0800

    .

--- lib/chat/server.tsx
+++ lib/chat/server.tsx
@@ -13,13 +13,13 @@ export async function submitMessage(msg: string, cId: string) {
     if (msg === "Is there a State Mandate Applicability for Diabetes for Individual Plans in Texas?") {
       await delay(1500);
       return {
-        response: `Yes, please visit http://localhost:3000/Texas%20-Mandated%20Beneifits%20%20-%20manhealthben.pdf then go to page 6`
+        response: `Yes, Texas has State Mandate Applicability for Diabetes for Individual Plans.`
       }
     }
     if (msg === "How about for Small Group Plans?") {
       await delay(1500);
       return {
-        response: "No"
+        response: `No, Texas doesn't offer State Mandate Applicability for Diabetes for Small Group Plans.`
       }
     }
 
@@ -27,6 +27,22 @@ export async function submitMessage(msg: string, cId: string) {
       await delay(1500);
       return {
         response: `Yes, please visit http://localhost:3000/Texas%20-Mandated%20Beneifits%20%20-%20manhealthben.pdf then go to page 6`
+      }
+    }
+
+    if (msg === "What's the Consumer Choice Plans for Texas?") {
+      await delay(1500);
+      return {
+        response: `please visit page 2 of http://localhost:3000/Texas%20-Mandated%20Beneifits%20%20-%20manhealthben.pdf `
+      }
+    }
+
+    if (msg === 'How to know if my plan is a "grandfathered" plan?') {
+      await delay(1500);
+      return {
+        response: `If your health plan has been the same since before March 23, 2010 (when Congress passed the ACA), your plan might be
+        “grandfathered” and exempt from some federal mandates. Additionally, some plans issued prior to 2014 are exempt from
+        EHB requirements under a transitional policy (https://www.tdi.texas.gov/health/faqs.html#transitionalrenewal).`
       }
     }
 
